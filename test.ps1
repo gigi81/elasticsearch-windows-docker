bf9e661d95a2ba8df15e1495412e25c1e52f053a
Luigi Grilli <luigi.grilli@gmail.com>
2019-07-06 12:23:21 +0100

    Trying to test with the actual container ip

--- test.ps1
+++ test.ps1
@@ -1,4 +1,7 @@
 ﻿Param (
+	[Parameter(Mandatory=$false)]
+   [string] $Timeout = 120
+   
    [Parameter(Mandatory=$false)]
    [string] $Timeout = 120
 )
@@ -6,12 +9,20 @@
 $start = [datetime]::UtcNow
 $successfull = $false
 
+$ip = docker inspect --format='{{.NetworkSettings.Networks.nat.IPAddress}}' elasticsearch
+$ip = $ip.Trim()
+
+if([string]::IsNullOrEmpty($ip))
+{
+    $ip = 'localhost'
+}
+
 while(!$successfull -and (New-TimeSpan -Start $start -End ([datetime]::UtcNow)).TotalSeconds -le $Timeout)
 {
     try
     {
         Write-Host "Getting cluster health..."
-        $health = Invoke-RestMethod 'http://127.0.0.1:9200/_cluster/health' -TimeoutSec 5
+        $health = Invoke-RestMethod 'http://$ip:9200/_cluster/health' -TimeoutSec 5
         Write-Host "Cluster status is $($health.status)"
         $successfull = $health.status -eq 'green'
     }
